622bf00680eabad235c53642e7f59fad8976f3c0
mcfalldm02 <mcfalldm@hotmail.com>
2017-04-19 20:35:07 -0500

    latest coide changes for week 13

--- script.js
+++ script.js
@@ -2,17 +2,20 @@
 // Dennis McFall
 // Spring 2017
 // Web233 Javascript
-// Date: 4/16/2017
+// Date: 4/19/2017
 // Assignment #13
-//Shopping List Version 4.2
+//Shopping List application
 
 /*
-It should have ability to create new cookie file from shoppinglist array
-It should have ability to read cookie file and update shoppinglist array
-It should have ability to delete cookie file 
-It should have ability to save new cookie file after displayshoppinglist function
-It should have ability to load & read cookie file function on Windows load
-It should have ability to populate shoppinglist array from cookie file
+1) Remove Cost Field
+
+2) Remove "Add" text next to checkbox
+
+3) Remove Version ?? from home page.
+
+4) Add ability to submit new item when [enter] key pressed
+
+5) Add popup describing app when visitors load webpage the first time
 */
 
 
@@ -24,15 +27,24 @@ var MyItems = {
 };
 
 
+function showAppDescription()
+{
+  var message = "";
+  
+  message = "Shopping List Phone Application\n\n";
+  message += " This convenient app was created during my javascript class - Spring 2017"
+}
+
 
 //In .JS file
 //v 4.0 save cookie
 //v 4.0 read cookie on load and display
 window.onload = function() {
-  debugger;
-  populateshoppinglistonload();
-   displayShoppinglists();
-};
+    alert("Welcome to the 'Shopping List' App");
+    populateshoppinglistonload();
+    displayShoppinglists();
+    clearFocus();
+}
 
 //In .JS file
 //v 4.0 populateshoppinglistonload()
@@ -41,46 +53,37 @@ function populateshoppinglistonload()
   shoppinglist = [];
   addtocart = [];
   //load cookie into array
-  var y = readCookie('mcfalllist'); 
+  var y = readCookie('mcfalllist');
   //remove unwanted chars and format
   y = remove_unwanted(y); 
-  //spit array by comma %2C
-  y = y.split('%2C');
-  if (y) {
-    shoppinglist = y;
-   }
 }
 
 
 
 //v 3.0 Update function addShoppinglist by adding objects
 function addShoppinglist(item) {
-  
+  //debugger;
   //v 3.0 declare variable for groc string
   var groc="";
   //v 3.0 v 3.0 declare variable for loop count
   var count=0;
   //v 3.0 edit value for MyItems.name
   MyItems.name=item;
+
   //v 3.0 for loop through object propterties and 
   for (var x in MyItems){
     //add to groc string from object array item
     groc += MyItems[x];
-    if (count===0){
-      groc += "  ";
-    }
     //increment count by 1
    count++;
   }
+  
   //push to shoppinglist
   shoppinglist.push(groc);
+  
   //display shoppinglist
   displayShoppinglists();
   
-  //v 4.0 save cookie
-  savecookie();
-  
-//v 2.1: call function 'clearFocus'
    clearFocus();
 }
 
@@ -89,15 +92,11 @@ function changeShoppinglist(position) {
   //document.getElementById("MyList").innerHTML = shoppinglist[position];
   var arrays = shoppinglist[position];
   arrays = arrays.split(",");
-    var e1 = arrays[0];
-   var e2 = arrays[1];
- var ReplacedAmount = e2.replace(/\$/g,'');
+  var e1 = arrays[0];
   var eitem = prompt("Please enter new item", e1);
-  shoppinglist[position] = eitem;
-  displayShoppinglists();
+  shoppinglist[position] = eitem + ",";
   
-  //v 4.0 save cookie
-  savecookie();
+  displayShoppinglists();
   
   displayShoppingCart();
   
@@ -110,10 +109,7 @@ function addbacktoshoppinglist(item,num) {
   shoppinglist.push(item);
   //display shoppinglist
   displayShoppinglists();
-  
-   //v 4.0 save cookie
-  savecookie();
-  
+
 //v3.1 display displayShoppingCart() 
   displayShoppingCart()
  
@@ -122,13 +118,12 @@ function addbacktoshoppinglist(item,num) {
 
 //v 3.1 Update function addShoppinglist by adding objects
 function addtoshopcart(item, num) {
+  //debugger;
     deleteShoppinglists(num);
     addtocart.push(item);
+    
   //display shoppinglist
   displayShoppinglists();
-  
-    //v 4.0 save cookie
-  savecookie();
 
 //v3.1 display displayShoppingCart() 
   displayShoppingCart();
@@ -139,38 +134,42 @@ function addtoshopcart(item, num) {
 
 
 function displayShoppinglists() {
-var TheList = "";
-var arrayLength = shoppinglist.length;
-for (var i = 0; i < arrayLength; i++) {
-var btndelete =  ' <input class="button" name="delete" type="button" value="Remove" onclick="deleteShoppinglists(' + i + ')" />';
-var arrays = shoppinglist[i];
-arrays = "'"+arrays+"'";
-var btnaddcart =  ' <input class="checkbox" name="add" type="checkbox" value="Add to Purchased List" onclick="addtoshopcart('+arrays+','+ i +')" />';
-TheList = btnaddcart + ' ' + TheList + shoppinglist[i] + btndelete + '<br>';
-}
-document.getElementById("MyList").innerHTML = '<h4>Shopping List</h4>' + '<br>' + TheList;
+  //debugger;
+  var TheList = "";
+  var arrayLength = shoppinglist.length;
+  for (var i = 0; i < arrayLength; i++) {
+    var arrays = shoppinglist[i];
+    arrays = "'"+arrays+"'";
+    var btnaddcart =  ' <input class="checkbox" name="add" type="checkbox" value="Add to Purchased List" onclick="addtoshopcart('+arrays+','+ i +')" />';
+    var btndelete =  ' <input class="button" name="delete" type="button" value="Remove" onclick="deleteShoppinglists(' + i + ')" />';
+
+    TheList = TheList + btnaddcart + ' ' + shoppinglist[i] + btndelete + '<br>';
+  }
+
+  document.getElementById("MyList").innerHTML = '<h4>Shopping List</h4>' + '<br>' + TheList;
+
 }
 
 function displayShoppingCart() {
-var TheList = "";
-var arrayLength = addtocart.length;
-for (var i = 0; i < arrayLength; i++) {
-var btndelete =  ' <input class="button" name="delete" type="button" value="Remove Item" onclick="deleteShoppingCart(' + i + ')" />';
-var arrays = addtocart[i];
-arrays = "'"+arrays+"'";
- var btnaddlist =  ' <input class="checkbox" name="add" type="checkbox" checked value="Add to Shopping List" onclick="addbacktoshoppinglist('+arrays+',' + i + ')" />';
-TheList = btnaddlist + ' ' + TheList + addtocart[i] + btndelete + '<br>';
-}
-document.getElementById("MyCart").innerHTML = '<h4>Shopping Cart</h4>' + '<br>' + TheList;
-}
+  //debugger;
+  var TheList = "";
+  var arrayLength = addtocart.length;
+  for (var i = 0; i < arrayLength; i++) { 
+    var arrays = addtocart[i];
+    arrays = "'"+arrays+"'";
+    var btnaddlist =  ' <input class="checkbox" name="add" type="checkbox" checked value="Add to Shopping List" onclick="addbacktoshoppinglist('+arrays+',' + i + ')" />';
+    var btndelete =  ' <input class="button" name="delete" type="button" value="Remove" onclick="deleteShoppingCart(' + i + ')" />';
+   
+    TheList = TheList + btnaddlist + ' ' + addtocart[i] + btndelete + '<br>';
+  }
 
+document.getElementById("MyCart").innerHTML = '<h4>Shopping Cart</h4>' + '<br>' + TheList;
+
+}
 
 function deleteShoppinglists(position) {
   shoppinglist.splice(position, 1);
   displayShoppinglists();
-  
-  //v 4.0 save cookie
-  savecookie();
   
   displayShoppingCart();
 }
@@ -179,9 +178,7 @@ function deleteShoppinglists(position) {
 function deleteShoppingCart(position) {
   addtocart.splice(position, 1);
   displayShoppinglists();
-  
-    //v 4.0 save cookie
-  savecookie();
+
 
   displayShoppingCart();
 
@@ -212,6 +209,7 @@ function savecookie()
 //In .JS file
 //v 4.0 read cookie and return
 function readCookie(name) {
+debugger;
     var nameEQ = name + "=";
     var ca = document.cookie.split(';');
     for(var i=0;i < ca.length;i++) {
@@ -242,108 +240,3 @@ function remove_unwanted(str) {
    str = str.replace(/%7C/g, " | ");
   return str.replace(/[^\x20-\x7E]/g, '');  
 } 
-
-
-
-
-
-/*
-
-
-Main();
-
-function Main()
-{
-  var end = 0;
-  var input;
-  var itemInput;
-  var itemNum;
-  var newItemName;
-  
-  var promptText = "Shopping List Application 2.0\n";
-  promptText += "Enter A to Add an item to the list\n";
-  promptText += "Enter C to Change an item in the list\n";
-  promptText += "Enter D to Delete an item from the list\n";
-  promptText += "Enter Q to Quit\n";
-  
-  do
-  {
-    input = prompt(promptText);
-    switch(input.toUpperCase())
-    {
-      case "A": //add item
-        itemInput = prompt("Enter a new item");
-        if(itemInput.length > 0)
-        {
-           addShoppinglistItem(itemInput);
-        }
-        else
-        {
-          alert("invalid input - name is 0 length");
-        }
-        break;
-      case "C": //change item 
-        itemNum = Number(prompt("Enter item number to change."));
-        //validate user input using regular expression
-        if(/[0-9]/.test(itemNum))
-        {
-            newItemName = prompt("Enter new item text.")
-            if(newItemName.length > 0)
-            {
-              changeShoppinglistItem(itemNum,newItemName);
-            }
-            else
-            {
-              alert("invalid input - name is 0 length");
-            }
-        }
-        else
-        {
-          alert("invalid input - not a number");
-        }
-        break;
-      case "D"://delete item
-         itemNum = Number(prompt("Enter item number to delete."));
-         if(!isNaN(itemNum))
-         {
-            deleteShoppinglistItem(itemNum);
-         }
-         else
-         {
-           alert("invalid input - not a number");
-         }
-        break;
-      case "Q"://quit program
-        displayShoppinglist();
-        end = 1;
-        break;
-      default:
-        displayShoppinglist();
-        end = 1;
-        break;
-    }
-    
-  }while(end === 0)
-  
-}
-
-function addShoppinglistItem(newItem)
-{
-    SHOPPINGLIST.push(newItem);
-}
-
-function changeShoppinglistItem(position, newValue) 
-{
-    SHOPPINGLIST[position] = newValue;
-}
- 
- 
-
-
-function displayShoppinglist()
-{
-    document.write("<div align='center'>" + formatDate() + "</div><br>");
-    document.write("<div align='center'>" + SHOPPINGLIST + "</div><br>");
-    document.write("<br><br>");
-}
-*/
